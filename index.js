0b056e469474332c8f0f6865bc699c7383aaea65
Tijhala Miller <tijhalamiller@Tijhalas-MacBook-Pro.local>
2025-01-09 16:59:32 -0500

    updated variables

--- index.js
+++ index.js
@@ -32,34 +32,46 @@ const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 console.log(isValid)
 
-
-const a = 55
-const b = 60
-const c = 75
-const mpg1= 30
-const mpg2 = 28
-const mpg3 = 23
-const tripmiles= 1500;
-const gascost= 3.00;
-const budget= 175.00;
+const trip_miles= 1500;
+let low_speed = 55
+let mid_speed= 60
+let high_speed= 75
+let mpg1= 30
+let mpg2 = 28
+let mpg3 = 23
+let gas_cost= 3.00;
+let budget= 175.00;
 
 // Total fuel gallons
-console.log(tripmiles / mpg1);
-console.log(tripmiles / mpg2);
-console.log(tripmiles / mpg3);
+let low_speed_fuel = Math.ceil(trip_miles / mpg1);
+let mid_speed_fuel = Math.ceil(trip_miles / mpg2);
+let high_speed_fuel = Math.ceil(trip_miles / mpg3);
+
+console.log(`At ${low_speed} mph, you need ${low_speed_fuel} gallons.`)
+console.log(`At ${mid_speed} mph, you need ${mid_speed_fuel} gallons.`)
+console.log(`At ${high_speed} mph, you need ${high_speed_fuel} gallons`)
 
 // Total Budget
-console.log(50 * gascost);
-console.log(53.57142857142857 * gascost);
-console.log(65.21739130434783 * gascost);
+let cheapest = low_speed_fuel * gas_cost
+let average = mid_speed_fuel * gas_cost
+let highest = high_speed_fuel * gas_cost
+
+console.log(`At a ${budget} dollar budget, the ${low_speed} mph trip costs ${cheapest} dollars`)
+console.log(`At a ${budget} dollar budget, the ${mid_speed} mph trip costs ${average} dollars`)
+console.log(`At a ${budget} dollar budget, the ${high_speed} mph trip costs ${highest} dollars`)
+
 
 // Total hours
-console.log(tripmiles / a);
-console.log(tripmiles / b);
-console.log(tripmiles / c);
 
-const besttrip=
-`At 55 mph,  50 gallons of fuel needed, cost is $150.00 and total trip time 27 hours. At 60 mph, about 54 gallons of fuel needed, cost is about $160.00 and total trip time is 25 hours. At 75 mph, 65 gallons of fuel needed, cost is $195.00 (over budget) and totatl trip time is 20 hours. The most cost effective trip is the first option.`
-console.log(besttrip);
+let fastest = trip_miles / high_speed
+let normal = trip_miles / mid_speed
+let slowest = trip_miles / low_speed
+
+console.log(`At ${low_speed} the trip will take ${slowest} hours`)
+console.log(`At ${mid_speed} the trip will take ${normal} hours`)
+console.log(`At ${high_speed} the trip will take ${fastest} hours`)
+
+
+console.log(`At ${low_speed} mph, ${low_speed_fuel} gallons of fuel is needed. The cost is ${cheapest} dollars and total trip time is ${slowest} hours. At ${mid_speed} mph, ${mid_speed_fuel} gallons of fuel is needed,. The cost is ${average} dollars and total trip time is ${normal} hours. At ${high_speed} mph, ${high_speed_fuel} gallons of fuel are needed. The cost is ${highest} dollars (over budget) and total trip time is ${fastest} hours. The most cost effective trip is Trip A which is ${cheapest} dollars but Trip B would allow you to arrive sooner, in ${normal} hours`);
 
 
